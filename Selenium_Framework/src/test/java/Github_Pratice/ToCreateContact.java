c8e26cb67a416aeebf0ec68d71597c4009de122e
Lenovo <Lenovo@LAPTOP-LU0D8EQR>
2024-07-20 19:50:08 +0530

    push1

---
+++ Selenium_Framework/src/test/java/Github_Pratice/ToCreateContact.java
@@ -0,0 +1,10 @@
+package Github_Pratice;
+
+public class ToCreateContact {
+
+	public static void main(String[] args) {
+		System.out.println("contact created successfully");
+		System.out.println("Contact saved");
+	}
+
+}
